01ead4bfa41f8b0ed57cb06f890d35f6bc7b96bd
Akshay <akshaybhople31@gmail.com>
2026-01-04 14:54:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup( 
+
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Akshay Bhople",
+    author_email="bhopleakshay31@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
